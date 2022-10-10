99279155101cadd024eccbd7a467656a3b4f9d74
Ginger90 <114303321+Ginger90@users.noreply.github.com>
2022-10-10 16:39:27 +0200

    Add files via upload

---
+++ js/assets/js/naming.js
@@ -0,0 +1,35 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+setInterval(function() {
+document.getElementById('gatto').innerHTML=petPreferito;
+}, 4000)
+
+var PetPreferito = 'coniglio';
+setInterval(function(){
+ document.getElementById('cane').innerHTML=PetPreferito;
+},4000)
+
+var $pet = 'giraffa';
+console.log($pet);
+setInterval(function()
+{ document.getElementById('gufo').innerHTML=$pet;
+},4000)
+
+var _pet = 'leone';
+console.log(_pet);
+setInterval(function()
+{ document.getElementById('cavallo').innerHTML=_pet;
+},4000)
+
+
+
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+
+
+//var 12 = 'numero';
